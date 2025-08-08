aa08dc587e9d2be9fbb8f952ff0333ade08b5255
prawnfairy <nakamototuna@yahoo.com>
2025-08-08 16:53:40 +0800

    Move Docker configuration files and .dockerignore into docker folder ; add ml app to settings.py

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework', # django rest framework
-    'apps.endpoints' # apps
+    'apps.endpoints', # apps
+    'apps.ml', # ml model
 ]
 
 MIDDLEWARE = [
