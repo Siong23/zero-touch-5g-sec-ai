e0069c567d4d771638d5dca8e700543f0ef33f76
prawnfairy <nakamototuna@yahoo.com>
2025-08-08 16:23:49 +0800

    Refactor code structure for improved readability and maintainability

---
+++ backend/server/apps/endpoints/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from apps.endpoints.views import EndpointViewSet, MLAlgorithmViewSet, MLAlgorithmStatusViewSet, MLRequestViewSet
+
+# Create REST API routers to the database model
+
+router = DefaultRouter(trailing_slash=False)
+router.register(r"endpoints", EndpointViewSet, basename="endpoints")
+router.register(r"mlalgorithm", MLAlgorithmViewSet, basename="mlalgorithm")
+router.register(r"mlalgorithmstatus", MLAlgorithmStatusViewSet, basename="mlalgorithmstatus")
+router.register(r"mlrequest", MLRequestViewSet, basename="mlrequest")
+
+urlpatterns = [path("api/v1/", include(router.urls)),]
