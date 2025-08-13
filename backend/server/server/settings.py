6cc180f80a23155351e5945069938ca1094e6c8b
prawnfairy <nakamototuna@yahoo.com>
2025-08-13 22:16:23 +0800

    Update allowed hosts

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7__d(a*74qx!mu@1n7*af0#*n-)cp6_v1=*r9h@==)rsx01-5b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
