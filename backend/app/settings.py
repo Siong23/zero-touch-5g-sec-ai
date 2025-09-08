424edde1ebe60d2b5f7fb4efcba810a958ab09f0
prawnfairy <nakamototuna@yahoo.com>
2025-09-08 12:30:29 +0800

    Add addresss for network host to receive network data

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -1,9 +1,9 @@
 # Configuration file to connect to the Open5gs
 OPEN5GS_CONFIG = {
     'HOST': '192.168.0.115',
-    'AMF_PORT': 38412,
+    'AMF_PORT': 7778, #38412
     'SMF_PORT': 7779,
-    'UPF_PORT': 7780,
+    #'UPF_PORT': 7780,
     'NETWORK_INTERFACE': 'ogstun',
-    'CAPTURE_FILTER': 'host{} or host{}'.format('AMF_IP', 'UPF_IP')
+    'CAPTURE_FILTER': 'host{} or host{}'.format('AMF_IP', 'SMF_IP')
 }
