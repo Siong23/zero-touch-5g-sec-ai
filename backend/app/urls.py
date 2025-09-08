649f74250e82f5e59792040ec45a3a0b004d7183
prawnfairy <nakamototuna@yahoo.com>
2025-09-08 11:16:58 +0800

    Add start_attack and stop_attack to the url path

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -7,5 +7,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('start_ml/', views.start_ml, name='start_ml'),
-    path('stop_ml/', views.stop_ml, name='stop_ml')
+    path('stop_ml/', views.stop_ml, name='stop_ml'),
+    path('start_attack/', views.start_attack, name='start_attack'),
+    path('stop_attack/', views.stop_attack, name='stop_attack')
 ]
