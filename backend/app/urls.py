95fb707b30a02a99faabf808c0c4b66558c86076
prawnfairy <nakamototuna@yahoo.com>
2025-09-08 14:57:14 +0800

    Configure network access settings

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('stop_ml/', views.stop_ml, name='stop_ml'),
     path('receive_network_data/', views.receive_network_data, name='receive_network_data'),
     path('start_attack/', views.start_attack, name='start_attack'),
-    path('stop_attack/', views.stop_attack, name='stop_attack')
+    path('stop_attack/', views.stop_attack, name='stop_attack'),
+    path('start_capture/', views.NetworkTrafficCapture.start_capture, name='start_capture'),
+    path('stop_capture', views.NetworkTrafficCapture.stop_capture, name='stop_capture'),
 ]
